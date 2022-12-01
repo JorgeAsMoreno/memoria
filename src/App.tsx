7c8c52113e8f3caede71ba8f4269cd5ed28e6c68
JorgeAsMoreno <solisamo6@gmail.com>
2022-12-01 12:38:48 -0600

    feat: create array images

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { cardsImages } from './data';
+import Card from './components/Card/Card';
 import './App.css';
 
+interface ICards {
+  src: string
+  animal: string
+}
+
 function App() {
+  const [cards, setCards] = useState<ICards[]>([])
+
+  useEffect(() => {
+    setCards(cardsImages)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        cards.map((card) => (
+          <Card />
+        ))
+      }
     </div>
   );
 }
---
+++ src/data.tsx
@@ -0,0 +1,75 @@
+import moose from './images/001.png'
+import bear from './images/001.png'
+import ferret from './images/003.png'
+import wolf from './images/004.png'
+import canary from './images/005.png'
+import seal from './images/006.png'
+import bird from './images/007.png'
+import whale from './images/008.png'
+
+export const cardsImages = [
+  {
+    src: moose,
+    animal: "moose"
+  },
+  {
+    src: moose,
+    animal: "moose"
+  },
+  {
+    src: bear,
+    animal: "bear"
+  },
+  {
+    src: bear,
+    animal: "bear"
+  },
+  {
+    src: ferret,
+    animal: "ferret"
+  },
+  {
+    src: ferret,
+    animal: "ferret"
+  },
+  {
+    src: wolf,
+    animal: "wolf"
+  },
+  {
+    src: wolf,
+    animal: "wolf"
+  },
+  {
+    src: canary,
+    animal: "canary"
+  },
+  {
+    src: canary,
+    animal: "canary"
+  },
+  {
+    src: seal,
+    animal: "seal"
+  },
+  {
+    src: seal,
+    animal: "seal"
+  },
+  {
+    src: bird,
+    animal: "bird"
+  },
+  {
+    src: bird,
+    animal: "bird"
+  },
+  {
+    src: whale,
+    animal: "whale"
+  },
+  {
+    src: whale,
+    animal: "whale"
+  },
+]
