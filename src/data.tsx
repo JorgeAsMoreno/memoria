3dc5b27b6cc03451f0ff3ad13f28e34c0f35a1b3
JorgeAsMoreno <solisamo6@gmail.com>
2022-12-01 18:03:55 -0600

    styles: add board and card styles

--- src/data.tsx
+++ src/data.tsx
@@ -9,66 +9,82 @@ import whale from './assets/images/008.png'
 
 export const cardsImages = [
   {
+    id: 1,
     frontFace: moose,
     name: "moose"
   },
   {
+    id: 2,
     frontFace: moose,
     name: "moose"
   },
   {
+    id: 3,
     frontFace: bear,
     name: "bear"
   },
   {
+    id: 4,
     frontFace: bear,
     name: "bear"
   },
   {
+    id: 5,
     frontFace: ferret,
     name: "ferret"
   },
   {
+    id: 6,
     frontFace: ferret,
     name: "ferret"
   },
   {
+    id: 7,
     frontFace: wolf,
     name: "wolf"
   },
   {
+    id: 8,
     frontFace: wolf,
     name: "wolf"
   },
   {
+    id: 9,
     frontFace: canary,
     name: "canary"
   },
   {
+    id: 10,
     frontFace: canary,
     name: "canary"
   },
   {
+    id: 11,
     frontFace: seal,
     name: "seal"
   },
   {
+    id: 12,
     frontFace: seal,
     name: "seal"
   },
   {
+    id: 13,
     frontFace: bird,
     name: "bird"
   },
   {
+    id: 14,
     frontFace: bird,
     name: "bird"
   },
   {
+    id: 15,
     frontFace: whale,
     name: "whale"
   },
   {
+    id: 16,
     frontFace: whale,
     name: "whale"
   },
